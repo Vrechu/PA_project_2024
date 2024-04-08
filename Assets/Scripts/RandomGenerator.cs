1a2a7cdf2f0b2302a30ee0242462e36313dabc9a
Freek Michielsen <freek.mich@gmail.com>
2024-04-08 13:33:22 +0200

    Add shape script

---
+++ Assets/Scripts/RandomGenerator.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This is the starting point for a custom random generator component
+///  (seeded pseudorandom).
+///  
+/// Note that the Shape and BuildTrigger classes call the three methods below.
+/// 
+/// TODO: Change this class such that whenever ResetRandom is called, a seeded pseudorandom generator is created,
+///  using the seed given in the inspector.
+///  (Possibly: use a "random" seed whenever seed=0.)
+/// </summary>
+public class RandomGenerator : MonoBehaviour
+{
+	public bool randomSeed;
+	public bool reuseSeed;
+	public int seed;
+	public int usedSeed;
+
+	System.Random rand = null;
+
+	/// <summary>
+	/// Returns a random integer between 0 and maxValue-1 (inclusive).
+	/// </summary>
+	public int Next(int maxValue)
+	{
+		return Rand.Next(maxValue);
+	}
+
+	public System.Random Rand
+	{
+		get
+		{
+			if (rand == null)
+			{
+				ResetRandom();
+			}
+			return rand;
+		}
+	}
+
+	public void ResetRandom()
+	{
+		if (!randomSeed)
+		{
+			rand = new System.Random(seed);
+			usedSeed = seed;
+		}
+		else
+		{
+			int tempSeed = new System.Random().Next();
+			rand = new System.Random(tempSeed);
+			usedSeed = tempSeed;
+		}
+	}
+}
